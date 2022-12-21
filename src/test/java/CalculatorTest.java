e59f67a8ac994c932ca0726d35d5c0ca8d78d33d
GleBukhanov <bestboyrare@gmail.com>
2022-12-21 13:00:10 +0300

    update README.md update Calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,29 +3,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator = new Calculator();
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(3, calculator.add(2, 1));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(4, calculator.dif(6, 2));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(3, calculator.div(6, 2));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(4, calculator.times(2, 2));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        assertEquals(19, calculator.solver(3, 5, 2, 10));
     }
 }
